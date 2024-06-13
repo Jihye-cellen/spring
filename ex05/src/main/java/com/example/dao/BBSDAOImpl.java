88e165865daab560c4fade2083e0372a426c1996
Jihye-cellen <ne4102@naver.com>
2024-06-13 18:08:28 +0900

    댓글

--- ex05/src/main/java/com/example/dao/BBSDAOImpl.java
+++ ex05/src/main/java/com/example/dao/BBSDAOImpl.java
@@ -55,4 +55,14 @@ public class BBSDAOImpl implements BBSDAO{
 		
 	}
 
+
+	@Override
+	public void updateReplycnt(int bid, int amount) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("bid", bid);
+		map.put("amount", amount);
+		session.update(namespace + ".updateReplycnt", map);
+		
+	}
+
 }
--- ex05/src/test/java/com/example/MysqlTest.java
+++ ex05/src/test/java/com/example/MysqlTest.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.dao.BBSDAO;
+import com.example.dao.ReplyDAO;
 import com.example.dao.UserDAO;
 import com.example.domain.QueryVO;
+import com.example.domain.ReplyVO;
 
 @SpringBootTest
 public class MysqlTest {
@@ -16,9 +18,20 @@ public class MysqlTest {
 	@Autowired
 	BBSDAO bdao;
 	
+	@Autowired
+	ReplyDAO rdao;
+	
 	@Test
 	public void test() {
-		bdao.updateViewcnt(250);
+//		ReplyVO vo = new ReplyVO();
+//		QueryVO query =new QueryVO();
+//		query.setPage(1);
+//		query.setSize(3);
+//		rdao.list(218, query);
+//		
+		
+		rdao.total(218);
+		
 	}
 
 }
